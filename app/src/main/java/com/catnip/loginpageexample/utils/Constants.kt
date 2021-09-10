2cc99ad2b3e08583bfc3c3f38abe595a2a04ad72
Muhammad Hermas Yuda P <Muhammad.pamungkas@alodokter.com>
2021-09-10 22:26:15 +0700

    Initial commit

---
+++ app/src/main/java/com/catnip/loginpageexample/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.catnip.loginpageexample.utils
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+object Constants {
+    const val DUMMY_USERNAME = "admin@gmail.com"
+    const val DUMMY_PASSWORD = "admin"
+}
